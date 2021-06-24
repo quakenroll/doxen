f174bb9f3f214aee8c7aedfd63b83419bf28660d
m <m>
2021-06-25 03:57:30 +0900

    ...

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/quakenroll/doxen
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.13.0 // indirect
+	github.com/99designs/gqlgen v0.13.0
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/golang-migrate/migrate/v4 v4.14.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/oauth2 v0.0.0-20210622215436-a8dc77f794b6 // indirect
 )
--- server.go
+++ server.go
@@ -1,29 +1,106 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/quakenroll/doxen/graph"
+	doxen "github.com/quakenroll/doxen/graph/generated"
+	"github.com/quakenroll/doxen/internal/auth"
+	database "github.com/quakenroll/doxen/internal/pkg/db/mysql"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/quakenroll/doxen/graph"
-	"github.com/quakenroll/doxen/graph/generated"
+	"github.com/go-chi/chi"
 )
 
-const defaultPort = "8080"
+const defaultPort = "8081"
+
+var googleOauthConfig = oauth2.Config{
+	RedirectURL:  "https://localhost:8081/auth/google/callback",
+	ClientID:     "1017766501798-6cq9t9iirccri93udjffg9s8a8esrn28.apps.googleusercontent.com",
+	ClientSecret: "ayoClDJ9bgDFNbjKPq8TzOO9",
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
+}
+
+type googleLoginHandler struct {
+}
+
+func (d googleLoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL("MyAuthVerificationString")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+type googleAuthCallbackHandler struct {
+}
+
+func (d googleAuthCallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	value := r.FormValue("state")
+	if value != "MyAuthVerificationString" {
+		log.Println("invalid google oauth state", value)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getGoogleUserInfo(r.FormValue("code"))
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprint(w, string(data))
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func getGoogleUserInfo(code string) ([]byte, error) {
+
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Exchange %s\n", err.Error())
+	}
+
+	resp, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Get UserInfo from Google %s\n", err.Error())
+	}
+	return ioutil.ReadAll(resp.Body)
+}
 
 func main() {
+
+	//generatepem.GenFile()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router := chi.NewRouter()
+	router.Use(auth.Middleware()) // this sets the handler that should be processed before any normal handler's routines
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(doxen.NewExecutableSchema(doxen.Config{Resolvers: &graph.Resolver{}}))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	router.Handle("/auth/google/login", googleLoginHandler{})
+	router.Handle("/auth/google/callback", googleAuthCallbackHandler{})
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to https://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServeTLS(":"+port, "localhost.crt", "localhost.key", router))
+
 }
